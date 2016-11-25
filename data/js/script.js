1044182b2f7d9755b427d0cfba2e265befdca593
Daniel Niquet <danielniquet@softr.net>
2016-11-25 13:25:46 -0600

    serviceworkers

--- data/js/script.js
+++ data/js/script.js
@@ -10,8 +10,12 @@ function onPermissionDenied () {
 }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/serviceWorker.js', {
-    scope: '/'
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
   });
 }
 
