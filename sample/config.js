b9c2de7e237e44cb914051b86b07b3f2ed16f8d4
Daniel Niquet <danielniquet@softr.net>
2016-11-25 13:50:19 -0600

    serviceworkers

--- sample/config.js
+++ sample/config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AIzaSyDZCRjv4Hg_7rUUBp2q44dIXTJwSx-z5ko'
+  gcmAPIKey: 'AAAAB2ME6BA:APA91bFyMOMjxxmAKFpQU0Eix2Fw-wkksSbbMrGQvSionxPTkldvyJk0x6nDvEBWBOoGnyHuM6HJ7YsMnLnYSCSBdNVJYsXu1Vy9tp_eZrPD8IU_ZCD9ipYjMAAI2QegQ2CIdCArOWj7'
 };
